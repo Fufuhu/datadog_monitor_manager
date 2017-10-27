4b869f4ebcb0d592a05cf4210a3ad7b853c3bf9e
Fufuhu <e238058r@hotmail.co.jp>
2017-10-27 20:30:43 +0900

    環境変数からのインポートを削除

--- datadog_monitor_manager/monitor_viewer/views/monitor_detail.py
+++ datadog_monitor_manager/monitor_viewer/views/monitor_detail.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from monitor_viewer.services.datadog_client import DatadogClient
-import os
 import json
 
 def get_detail(request,monitor_id=None):
     # return HttpResponse('リスト')
-    client = DatadogClient(api_key=os.getenv('DD_API_KEY'),app_key=os.getenv('DD_APP_KEY'))
+    client = DatadogClient(api_key=request.session.get('api_key'),
+                           app_key=request.session.get('app_key'))
     monitor = client.get_monitor(monitor_id=monitor_id)
     dumped_monitor = json.dumps(monitor, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': '))
 
