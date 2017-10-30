dbef7a2556e071be27fbc82316e11dfe46acb4ed
Fufuhu <e238058r@hotmail.co.jp>
2017-10-30 17:31:16 +0900

    キー情報が不正な場合にキー情報の設定画面に遷移するよう設定

--- datadog_monitor_manager/monitor_viewer/views/monitor_list.py
+++ datadog_monitor_manager/monitor_viewer/views/monitor_list.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from monitor_viewer.services.datadog_client import DatadogClient
 
@@ -9,5 +9,11 @@ def get_list(request):
     client = DatadogClient(api_key=request.session.get('api_key'),
                            app_key=request.session.get('app_key'))
     monitors = client.get_monitors()
+
+    if 'errors' in monitors:
+        param = {
+            'is_error': True,
+        }
+        return render(request, 'monitor_settings.html', {'param': param})
 
     return render(request, 'monitor_list.html',{'monitors': monitors})
